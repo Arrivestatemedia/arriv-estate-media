b24477ec3368567ab040ac183fd5fd4e577b138f
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-11 16:17:55 +0000

    File changes

---
+++ src/components/GoogleMapsLoader.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+export default function GoogleMapsLoader() {
+  useEffect(() => {
+    if (window.google) return;
+
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+  }, []);
+
+  return null;
+}
