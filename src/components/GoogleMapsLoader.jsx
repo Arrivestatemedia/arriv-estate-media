7744c4be0885e52233ec3347d5dc83b76b3d5a30
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-11 16:52:56 +0000

    File changes

--- src/components/GoogleMapsLoader.jsx
+++ src/components/GoogleMapsLoader.jsx
@@ -4,8 +4,10 @@ export default function GoogleMapsLoader() {
   useEffect(() => {
     if (window.google) return;
 
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+    console.log("Google Maps API Key being used:", apiKey);
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
     script.async = true;
     script.defer = true;
     document.head.appendChild(script);
