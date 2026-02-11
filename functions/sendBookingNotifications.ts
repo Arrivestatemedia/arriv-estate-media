0058215452d9bb9e7b16b9dc6153dc42736ac64c
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-11 15:29:43 +0000

    File changes

--- functions/sendBookingNotifications.ts
+++ functions/sendBookingNotifications.ts
@@ -41,11 +41,43 @@ Deno.serve(async (req) => {
             }
         }
         
-        // Send email confirmation
-        await base44.integrations.Core.SendEmail({
-            to: booking.client_email,
-            subject: 'Booking Confirmation - Arriv',
-            body: `Hi ${booking.client_name},\n\nYour booking has been confirmed!\n\nProperty: ${booking.property_address}\nDate: ${booking.preferred_date}\nTime: ${booking.preferred_time}\nPackage: ${booking.package}\n\nYou should receive a calendar invite shortly. We'll contact you if there are any changes.\n\nThank you,\nArriv Team`
+        // Send email confirmation via Gmail
+        const adminEmail = 'BradCBurke@arrivestatemedia.com';
+        const accessToken = await base44.asServiceRole.connectors.getAccessToken('gmail');
+
+        const emailSubject = 'Your Booking Confirmation - Arriv';
+        const emailBody = `Hi ${booking.client_name},\n\nYour booking has been confirmed!\n\nProperty: ${booking.property_address}\nDate: ${booking.preferred_date}\nTime: ${booking.preferred_time}\nPackage: ${booking.package}\n\nYou should receive a calendar invite shortly. We'll contact you if there are any changes.\n\nThank you,\nArriv Team`;
+
+        const messageLines = [
+            `To: ${booking.client_email}`,
+            `From: ${adminEmail}`,
+            `Subject: ${emailSubject}`,
+            'MIME-Version: 1.0',
+            'Content-Type: text/plain; charset="UTF-8"',
+            '',
+            emailBody
+        ];
+
+        const messageParts = messageLines.map(line => new TextEncoder().encode(line + '\r\n'));
+        const messageBytes = messageParts.reduce((acc, part) => {
+            const newAcc = new Uint8Array(acc.length + part.length);
+            newAcc.set(acc);
+            newAcc.set(part, acc.length);
+            return newAcc;
+        }, new Uint8Array());
+
+        const base64urlMessage = btoa(String.fromCharCode(...messageBytes))
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_')
+            .replace(/=/g, '');
+
+        await fetch('https://www.googleapis.com/gmail/v1/users/me/messages/send', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ raw: base64urlMessage })
         });
 
         return Response.json({ success: true });
