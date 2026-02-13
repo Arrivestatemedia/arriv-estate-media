3131efcafcffc08cbf646a246afcf0c772d4ca69
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-13 16:29:26 +0000

    File changes

--- functions/sendJobReminders.ts
+++ functions/sendJobReminders.ts
@@ -135,11 +135,19 @@ Deno.serve(async (req) => {
 
           // Send email to media partner
           if (job.booked_by) {
-            await base44.asServiceRole.integrations.Core.SendEmail({
-              to: job.booked_by,
-              subject: 'Shoot Reminder - Today at ' + jobTime,
-              body: emailBody
-            });
+            try {
+              if (gmailAccessToken) {
+                await sendEmailViaGmail(gmailAccessToken, job.booked_by, 'Shoot Reminder - Today at ' + jobTime, emailBody);
+              } else {
+                await base44.asServiceRole.integrations.Core.SendEmail({
+                  to: job.booked_by,
+                  subject: 'Shoot Reminder - Today at ' + jobTime,
+                  body: emailBody
+                });
+              }
+            } catch (e) {
+              console.log('Email send skipped:', e.message);
+            }
             await base44.asServiceRole.entities.MessageLog.create({
               message_type: 'email',
               recipient_type: 'media_partner',
