a2c1b06151c3c68aacb136fb66488649123b3abd
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-13 16:37:04 +0000

    File changes

--- functions/sendJobReminders.ts
+++ functions/sendJobReminders.ts
@@ -119,8 +119,9 @@ Deno.serve(async (req) => {
         // Send appropriate reminders
         if (reminder.type === '9am_morning') {
           // Send to client and media partner
-          const message = `Good Morning ${job.client_name}!\nJust confirming our shoot today at ${jobTime} at your ${job.location} listing. Looking forward to it.\n-Brad`;
-          const emailBody = `Good Morning ${job.client_name}!\n\nJust confirming our shoot today at ${jobTime} at your ${job.location} listing. Looking forward to it.\n\n-Brad`;
+          const jobTime12 = convertTo12HourFormat(jobTime);
+          const message = `Good Morning ${job.client_name}!\nJust confirming our shoot today at ${jobTime12} at your ${job.location} listing. Looking forward to it.\n-Brad`;
+          const emailBody = `Good Morning ${job.client_name}!\n\nJust confirming our shoot today at ${jobTime12} at your ${job.location} listing. Looking forward to it.\n\n-Brad`;
 
           // Send to media partner
           if (job.booked_by_phone) {
