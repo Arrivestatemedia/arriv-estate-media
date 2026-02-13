2f580885563bf58baed196b33a56313e58405417
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-13 08:14:36 +0000

    File changes

--- functions/createSampleStatement.ts
+++ functions/createSampleStatement.ts
@@ -54,22 +54,6 @@ Deno.serve(async (req) => {
       gigs_completed: 5,
       gross_amount: 850.00,
       payout_method: 'zelle',
-      payout_destination: 'ilimbooking@gmail.com'
-    });
-
-    // Create data URL from PDF
-    const pdfDataUrl = `data:application/pdf;base64,${pdfRes.data.pdf_data}`;
-
-    // Create payment statement record
-    const statement = await base44.asServiceRole.entities.PaymentStatement.create({
-      media_partner_email: 'ilimbooking@gmail.com',
-      media_partner_name: 'Sample Media Partner',
-      payout_date: '2026-02-13',
-      period_start: '2026-02-08',
-      period_end: '2026-02-12',
-      gigs_completed: 5,
-      gross_amount: 850.00,
-      payout_method: 'zelle',
       payout_destination: 'ilimbooking@gmail.com',
       pdf_url: pdfDataUrl,
       is_archived: false
