8d59e63efe35e0296ece4bc477ac4f76b6354bc4
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-13 09:46:06 +0000

    File changes

--- functions/updateBooking.ts
+++ functions/updateBooking.ts
@@ -18,6 +18,46 @@ Deno.serve(async (req) => {
 
         const updatedBooking = await base44.asServiceRole.entities.Booking.update(bookingId, updates);
 
+        // Send approval email to client
+        const accessToken = await base44.asServiceRole.connectors.getAccessToken('gmail');
+        const adminEmail = 'BradCBurke@arrivestatemedia.com';
+        
+        const propertyAddress = `${updatedBooking.street_address}, ${updatedBooking.city}, ${updatedBooking.state}`;
+        const emailBody = `Hi ${updatedBooking.client_name},\n\nYour booking request has been approved and updated.\n\nProperty: ${propertyAddress}\nDate: ${updatedBooking.preferred_date}\nTime: ${updatedBooking.preferred_time}\n\nThank you,\nArriv Team`;
+        const emailSubject = 'Booking Approved - Arriv';
+
+        const messageLines = [
+            `To: ${updatedBooking.client_email}`,
+            `From: ${adminEmail}`,
+            `Subject: ${emailSubject}`,
+            'MIME-Version: 1.0',
+            'Content-Type: text/plain; charset="UTF-8"',
+            '',
+            emailBody
+        ];
+
+        const messageParts = messageLines.map(line => new TextEncoder().encode(line + '\r\n'));
+        const messageBytes = messageParts.reduce((acc, part) => {
+            const newAcc = new Uint8Array(acc.length + part.length);
+            newAcc.set(acc);
+            newAcc.set(part, acc.length);
+            return newAcc;
+        }, new Uint8Array());
+
+        const base64urlMessage = btoa(String.fromCharCode(...messageBytes))
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_')
+            .replace(/=/g, '');
+
+        await fetch('https://www.googleapis.com/gmail/v1/users/me/messages/send', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ raw: base64urlMessage })
+        });
+
         return Response.json({ success: true, booking: updatedBooking });
     } catch (error) {
         console.error('Update booking error:', error);
