d21387a9e8e8cf8e1d339ede164b0c86505bd53e
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-13 04:22:25 +0000

    File changes

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,7 +4,7 @@ import { base44 } from "@/api/base44Client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { LogIn } from "lucide-react";
+import { LogIn, Eye, EyeOff } from "lucide-react";
 import { createPageUrl } from "../utils";
 
 export default function SignIn() {
