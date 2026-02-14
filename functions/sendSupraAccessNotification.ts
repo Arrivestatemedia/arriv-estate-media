1a41d48446a4085533244f950b5642a049a97f95
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-14 15:19:05 +0000

    File changes

--- functions/sendSupraAccessNotification.ts
+++ functions/sendSupraAccessNotification.ts
@@ -41,7 +41,8 @@ Deno.serve(async (req) => {
     const formattedTime = job.start_time || '9:00 AM';
 
     // Create SMS message
-    const smsMessage = `Your Photographer/Videographer ${job.booked_by_name} will be seeing you on ${formattedDate} at ${formattedTime}. If you do not plan on being on site please make sure that you have granted Supra access to ${bookedByPhone}. Supra instructions: https://drive.google.com/file/d1mtMMXNAIutztKxa4GYrqrWx96uoEGqRv/view?usp=drivesdk`;
+    const clientFirstName = job.client_name.split(' ')[0];
+    const smsMessage = `Hi ${clientFirstName}!\n\nYour Media Specialist ${job.booked_by_name} will be seeing you on ${formattedDate} at ${formattedTime}.\n\nIf you do not plan on being on site please make sure that you have granted Supra access to the number below:\n\n${bookedByPhone}\n\nPlease see attached document for instructions on how to add Temporary access in Supra.\n\nSupra instructions:\nhttps://bit.ly/3OdUc80`;
 
     // Send SMS via Twilio
     const accountSid = Deno.env.get('TWILIO_ACCOUNT_SID');
