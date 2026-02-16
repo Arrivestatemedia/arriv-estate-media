43ca1a87a8322f354c6bfe6a204add7b476d77b3
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-16 08:00:07 +0000

    File changes

--- src/components/auth/GoogleSignInButton.jsx
+++ src/components/auth/GoogleSignInButton.jsx
@@ -13,7 +13,7 @@ export default function GoogleSignInButton({ onPhoneNumberNeeded, userType, disa
         return;
       }
 
-      const redirectUri = `${window.location.origin}/auth/google/callback`;
+      const redirectUri = `${window.location.origin}/signin/google/callback`;
       const scope = "openid profile email";
       const responseType = "code";
 
