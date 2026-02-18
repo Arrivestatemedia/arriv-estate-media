e7e205405e612a00aba81c152901aaeaf006e957
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-18 00:20:40 +0000

    File changes

--- src/pages/OrientationVideo.jsx
+++ src/pages/OrientationVideo.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { base44 } from "@/api/base44Client";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { createPageUrl } from "../utils";
