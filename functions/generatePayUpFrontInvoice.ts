8694e84f44600d0aaddb5b4b8fa67d3a56a3e9a5
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-18 05:06:15 +0000

    File changes

--- functions/generatePayUpFrontInvoice.ts
+++ functions/generatePayUpFrontInvoice.ts
@@ -83,8 +83,7 @@ Deno.serve(async (req) => {
     };
 
     // Upload invoice to Google Drive UNPAID folder (async, don't block)
-    let googleDriveUrl = stripeData.url;
-    let googleDriveFileId = 'temp';
+    // Don't set initial values - let Drive upload update them
     base44.asServiceRole.functions.invoke('uploadInvoiceToGoogleDrive', {
       fileName: `Invoice_${invoiceNumber}_${booking.client_name.replace(/\s+/g, '_')}.txt`,
       invoiceContent: `INVOICE #${invoiceNumber}\n\nClient: ${booking.client_name}\nProperty: ${jobAddress}\nService Date: ${booking.preferred_date}\n\nAmount Due: $${totalAmount}\n\nPayment Link: ${stripeData.url}`,
