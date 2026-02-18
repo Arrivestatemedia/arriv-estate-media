3ca99125b66207fbb9828b763f1fbb8defc32605
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-18 05:32:53 +0000

    File changes

--- functions/generatePayUpFrontInvoice.ts
+++ functions/generatePayUpFrontInvoice.ts
@@ -84,13 +84,16 @@ Deno.serve(async (req) => {
     // Upload invoice to Google Drive UNPAID folder BEFORE creating invoice record
     let googleDriveUrl = null;
     let googleDriveFileId = null;
+    console.log('Starting Google Drive upload...');
     try {
+      console.log('Invoking uploadInvoiceToGoogleDrive function...');
       const driveResult = await base44.functions.invoke('uploadInvoiceToGoogleDrive', {
         fileName: `Invoice_${invoiceNumber}_${booking.client_name.replace(/\s+/g, '_')}.txt`,
         invoiceContent: `INVOICE #${invoiceNumber}\n\nClient: ${booking.client_name}\nProperty: ${jobAddress}\nService Date: ${booking.preferred_date}\n\nAmount Due: $${totalAmount}\n\nPayment Link: ${stripeData.url}`,
         folderType: 'unpaid'
       });
 
+      console.log('Drive result received:', JSON.stringify(driveResult.data));
       if (driveResult.data?.fileUrl) {
         googleDriveUrl = driveResult.data.fileUrl;
         googleDriveFileId = driveResult.data.fileId;
@@ -99,6 +102,7 @@ Deno.serve(async (req) => {
     } catch (driveError) {
       console.error('Drive upload error:', driveError.message);
     }
+    console.log('Google Drive URL after upload:', googleDriveUrl);
 
     // Create invoice record with Google Drive URL already populated
     const invoice = await base44.asServiceRole.entities.Invoice.create({
