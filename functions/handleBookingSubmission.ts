57ef30b21a6eb83dc384133c23bf3e38694aae7d
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-19 03:23:23 +0000

    Update complete.

--- functions/handleBookingSubmission.ts
+++ functions/handleBookingSubmission.ts
@@ -134,31 +134,7 @@ Deno.serve(async (req) => {
           'DATE OF JOB': booking.preferred_date,
         });
 
-        // Inject a clickable hyperlink for the Stripe URL directly into the DOCX XML
-        // We find the run containing the Stripe URL and wrap it with a proper hyperlink relationship
-        const renderedZip = doc.getZip();
-
-        // Add relationship for hyperlink
-        const relsPath = 'word/_rels/document.xml.rels';
-        let relsXml = renderedZip.files[relsPath]?.asText() || '';
-        const hyperlinkRelId = 'rIdStripePayNow';
-        const hyperlinkRel = `<Relationship Id="${hyperlinkRelId}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink" Target="${stripeUrl}" TargetMode="External"/>`;
-        relsXml = relsXml.replace('</Relationships>', `${hyperlinkRel}</Relationships>`);
-        renderedZip.file(relsPath, relsXml);
-
-        // Find the text run in document.xml that contains the Stripe URL and wrap it in a hyperlink element
-        let docXml = renderedZip.files['word/document.xml']?.asText() || '';
-        // Escape stripeUrl for XML search
-        const escapedUrl = stripeUrl.replace(/&/g, '&amp;');
-        // Replace the run containing the stripe URL with a hyperlink element
-        // The run will look like: <w:r>...<w:t>https://buy.stripe.com/...</w:t>...</w:r>
-        docXml = docXml.replace(
-          new RegExp(`(<w:r[^>]*>(?:<w:rPr>[^<]*(?:<[^>]+>[^<]*<\\/[^>]+>)*<\\/w:rPr>)?<w:t[^>]*>)(${escapedUrl.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})(</w:t></w:r>)`),
-          `<w:hyperlink r:id="${hyperlinkRelId}" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"><w:r><w:rPr><w:rStyle w:val="Hyperlink"/><w:b/><w:sz w:val="28"/><w:szCs w:val="28"/></w:rPr><w:t>Pay Now</w:t></w:r></w:hyperlink>`
-        );
-        renderedZip.file('word/document.xml', docXml);
-
-        const updatedDocx = renderedZip.generate({ type: 'uint8array', compression: 'DEFLATE' });
+        const updatedDocx = doc.getZip().generate({ type: 'uint8array', compression: 'DEFLATE' });
 
         // Upload filled DOCX to Google Drive as Google Doc (auto-converts to Google Doc format)
         console.log('Uploading filled DOCX to Google Drive...');
