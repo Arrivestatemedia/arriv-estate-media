dd7e4a1e105f308e950a16b5e7d3f5bb71d08a8d
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-20 06:03:34 +0000

    File changes

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -186,10 +186,25 @@ export default function BookingPage() {
   const totalPrice = (selectedPackage?.price || 0) + cartAddOns.reduce((sum, a) => sum + a.price, 0);
 
   const handleSubmitBooking = async (bookingData) => {
+    if (requestPayAtClosing) {
+      setShowPayAtClosingDialog(true);
+      return;
+    }
     return new Promise((resolve) => {
       createBookingMutation.mutate({
         ...bookingData,
-        request_pay_at_closing: requestPayAtClosing
+        request_pay_at_closing: false
+      }, {
+        onSettled: () => resolve(),
+      });
+    });
+  };
+
+  const handleConfirmPayAtClosing = async (bookingData) => {
+    return new Promise((resolve) => {
+      createBookingMutation.mutate({
+        ...bookingData,
+        request_pay_at_closing: true
       }, {
         onSettled: () => resolve(),
       });
