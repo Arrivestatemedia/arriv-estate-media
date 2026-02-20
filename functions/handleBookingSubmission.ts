c96d9ccf842f85ef7358ea5c50c4b5021ed11598
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-20 02:36:29 +0000

    File changes

--- functions/handleBookingSubmission.ts
+++ functions/handleBookingSubmission.ts
@@ -147,7 +147,7 @@ Deno.serve(async (req) => {
         doc.setDrawColor(184, 149, 106);
         doc.setLineWidth(1);
         doc.line(margin, curY, pageWidth - margin, curY);
-        curY += 20;
+        curY += 30;
 
         // INVOICE title
         doc.setFont('helvetica', 'bold');
