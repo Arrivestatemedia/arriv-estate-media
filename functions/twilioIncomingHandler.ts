af049b98cf4f86d3375cfbd790644aead8449a90
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-22 04:49:25 +0000

    File changes

--- functions/twilioIncomingHandler.ts
+++ functions/twilioIncomingHandler.ts
@@ -9,11 +9,15 @@ Deno.serve(async (req) => {
     const from = params.get('From');
     const to = params.get('To');
     
+    // Get the sales rep's phone number from Twilio number
+    const salesMembers = await base44.asServiceRole.entities.SalesTeamMember.filter({ twilio_phone_number: to });
+    const salesMemberPhone = salesMembers.length > 0 ? salesMembers[0].phone_number : Deno.env.get('BRADLEY_PHONE');
+    
     // TwiML to handle incoming call
     const twiml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Say voice="alice">You have an incoming call from the dialer. Please hold while we connect you.</Say>
-  <Dial>${to}</Dial>
+  <Say voice="alice">You have an incoming call. Please hold while we connect you.</Say>
+  <Dial>${salesMemberPhone}</Dial>
 </Response>`;
 
     return new Response(twiml, {
