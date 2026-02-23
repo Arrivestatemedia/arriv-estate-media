de2f3c89860f9bfb6db3203c6e044cc3db67d25e
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-23 02:22:17 +0000

    File changes

--- src/pages/HubSpotActivityLog.jsx
+++ src/pages/HubSpotActivityLog.jsx
@@ -288,7 +288,7 @@ export default function HubSpotActivityLog() {
                 </div>
               </DialogContent>
             </Dialog>
-          )}
+            )}
           </div>
         </div>
 
