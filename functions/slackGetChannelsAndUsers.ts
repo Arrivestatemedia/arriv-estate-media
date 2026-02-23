f1c28337414237aa747a5a7312bc7880f301aca3
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-23 20:47:37 +0000

    File changes

--- functions/slackGetChannelsAndUsers.ts
+++ functions/slackGetChannelsAndUsers.ts
@@ -3,11 +3,6 @@ import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 Deno.serve(async (req) => {
     try {
         const base44 = createClientFromRequest(req);
-        const user = await base44.auth.me();
-
-        if (!user) {
-            return Response.json({ error: 'Unauthorized' }, { status: 401 });
-        }
 
         const accessToken = await base44.asServiceRole.connectors.getAccessToken('slack');
 
