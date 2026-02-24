78933c3bb58de175ebd46660140359c98d0fa289
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-24 21:09:09 +0000

    File changes

--- src/components/sales/EmailComposer.jsx
+++ src/components/sales/EmailComposer.jsx
@@ -510,13 +510,12 @@ export default function EmailComposer({ salesMemberId, isAdmin = false }) {
                  >
                    Cancel
                  </Button>
-               </div>
-               </div>
-               </div>
-               </div>
-               )}
-               </div>
-               )}
+                 </div>
+                 </div>
+                 </div>
+                 )}
+                 </div>
+                 )}
 
       {/* ── SCHEDULED ── */}
       {tab === "scheduled" && (
