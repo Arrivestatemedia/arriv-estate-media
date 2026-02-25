9005f09e9cbe3221a5ab9672148fcc8e24fec3c8
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-02-25 03:58:47 +0000

    File changes

--- src/pages/HubSpotActivityLog.jsx
+++ src/pages/HubSpotActivityLog.jsx
@@ -786,11 +786,12 @@ export default function HubSpotActivityLog() {
         </Dialog>
 
         <PoweredByFooter />
+      </div>
 
-        {/* Floating chat bubble - shown when NOT on chat tab */}
-        {activeTab !== "chat" && (
-          <FloatingChatBubble currentUserId={user?.id} currentUserName={user?.full_name} />
-        )}
-        </div>
-    );
+      {/* Floating chat bubble - shown when NOT on chat tab */}
+      {activeTab !== "chat" && (
+        <FloatingChatBubble currentUserId={user?.id} currentUserName={user?.full_name} />
+      )}
+    </div>
+  );
 }
