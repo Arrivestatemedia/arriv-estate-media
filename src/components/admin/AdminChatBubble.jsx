779ecdd2add0bace78464911bbceb80caeb352a4
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-01 17:40:47 +0000

    File changes

--- src/components/admin/AdminChatBubble.jsx
+++ src/components/admin/AdminChatBubble.jsx
@@ -46,7 +46,7 @@ export default function AdminChatBubble({ currentUserId, currentUserName, onInit
   // When opened, don't show badge
   const displayCount = open ? 0 : unreadCount;
   // Position changes based on video call state
-  const isOnRight = !isVideoActive;
+  const isOnRight = !videoState;
 
   // Standard floating chat bubble (chat panel opens when clicked)
   return (
