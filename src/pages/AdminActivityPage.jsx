912ce4f7ee5ff3489dcae8b007d584c85aace864
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-01 01:58:30 +0000

    File changes

--- src/pages/AdminActivityPage.jsx
+++ src/pages/AdminActivityPage.jsx
@@ -209,6 +209,21 @@ export default function AdminActivityPage({ user }) {
     }
   });
 
+  const handleAcceptVideoCall = async () => {
+    if (!incomingVideoCall) return;
+    setVideoCallProcessing(true);
+    await base44.entities.PendingNotification.update(incomingVideoCall.notificationId, { is_read: true }).catch(() => {});
+    setActiveVideoCall(incomingVideoCall);
+    setIncomingVideoCall(null);
+    setVideoCallProcessing(false);
+  };
+
+  const handleDeclineVideoCall = async () => {
+    if (!incomingVideoCall) return;
+    await base44.entities.PendingNotification.update(incomingVideoCall.notificationId, { is_read: true }).catch(() => {});
+    setIncomingVideoCall(null);
+  };
+
   const activityIcons = {
     call: <Phone className="w-4 h-4" />,
     email: <Mail className="w-4 h-4" />,
