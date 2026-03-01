344b221ad5855a59cfd8333fd46249acf9f356c1
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-01 22:12:09 +0000

    File changes

--- src/pages/AdminActivityPage.jsx
+++ src/pages/AdminActivityPage.jsx
@@ -922,7 +922,7 @@ export default function AdminActivityPage({ user: propsUser, onVideoCallStateCha
            />
          )}
 
-        {/* Chat bubble - hide during active video call unless unread messages */}
+        {/* Chat bubble - always visible */}
          <AdminChatBubble
            currentUserId={user?.id} 
            currentUserName={user?.full_name}
@@ -939,7 +939,7 @@ export default function AdminActivityPage({ user: propsUser, onVideoCallStateCha
                }
              }).catch(() => {});
            }}
-           isVideoCallActive={isVideoWindowOpen && !!activeVideoCall}
+           isVideoCallActive={false}
          />
 
         {/* Minimized video call indicator */}
