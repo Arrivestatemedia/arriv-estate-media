e26e021ec6ce2ab454848c21aebdfc40910b89ba
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-01 03:37:21 +0000

    File changes

--- src/pages/HubSpotActivityLog.jsx
+++ src/pages/HubSpotActivityLog.jsx
@@ -784,6 +784,7 @@ export default function HubSpotActivityLog() {
           <FloatingChatBubble
             currentUserId={user?.id}
             currentUserName={user?.full_name}
+            isVideoActive={!!activeVideoCall}
             onInitiateTransfer={(memberId, memberName) => {
               base44.entities.SalesTeamMember.filter({ id: memberId }).then(members => {
                 const ext = members?.[0]?.extension;
