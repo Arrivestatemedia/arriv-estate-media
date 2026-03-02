d6d9506278fab1871c166f2d7251738b1f2ec1de
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-02 05:07:53 +0000

    File changes

--- src/pages/AdminHub.jsx
+++ src/pages/AdminHub.jsx
@@ -390,12 +390,13 @@ export default function AdminHub() {
         <div>show: {String((callStatus === 'idle') || hasUnreadNotification)}</div>
       </div>
 
-      {/* Admin floating chat bubble - hidden during live call */}
+      {/* Admin floating chat bubble - hidden during live call, disabled when video call active */}
       {((callStatus === 'idle') || hasUnreadNotification) && (
         <AdminChatBubble
           currentUserId={user.id}
           currentUserName={user.full_name}
           isVideoActive={false}
+          disabled={isInLiveCall}
           onInitiateTransfer={(memberId, memberName) => {
             base44.entities.SalesTeamMember.filter({ id: memberId }).then(members => {
               const ext = members?.[0]?.extension;
