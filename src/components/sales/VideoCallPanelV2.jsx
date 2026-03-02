ca7df81ae2d9c3ced9001708cb2295b1165ad9ab
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-02 03:42:18 +0000

    File changes

--- src/components/sales/VideoCallPanelV2.jsx
+++ src/components/sales/VideoCallPanelV2.jsx
@@ -560,19 +560,6 @@ export default function VideoCallPanelV2({
             onEndCall={handleEndCall}
             onSettings={() => setIsSettingsOpen(true)}
           />
-          <Button
-            size="icon"
-            onClick={() => {
-              setIsChatOpen(v => !v);
-              onChatOpenRequest?.();
-            }}
-            className={`h-10 w-10 rounded-full flex-shrink-0 ${
-              isChatOpen ? "bg-blue-600 hover:bg-blue-700" : "bg-gray-700 hover:bg-gray-600"
-            }`}
-            title="Chat"
-          >
-            <MessageCircle className="w-5 h-5 text-white" />
-          </Button>
         </div>
       </div>
 
