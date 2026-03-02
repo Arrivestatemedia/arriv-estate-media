21e20a49873fc6d7316ffa6dae8f8b97ec3ff0a3
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-02 02:29:56 +0000

    File changes

--- src/pages/AdminActivityPage.jsx
+++ src/pages/AdminActivityPage.jsx
@@ -525,6 +525,8 @@ export default function AdminActivityPage({ user: propsUser, onVideoCallStateCha
             currentUserName={user?.full_name} 
             salesMemberId={user?.id} 
             isAdmin={true}
+            onVideoCallStarted={() => setActiveVideoCall({ callerName: "Video Call" })}
+            onVideoCallEnded={() => setActiveVideoCall(null)}
             onInitiateTransfer={(memberId, memberName) => {
               base44.entities.SalesTeamMember.filter({ id: memberId }).then(members => {
                 const ext = members?.[0]?.extension;
