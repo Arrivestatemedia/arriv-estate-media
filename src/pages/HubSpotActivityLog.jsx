ec5ee2efaae91fc14faa0d118b9924d662c284f6
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-03 21:50:26 +0000

    File changes

--- src/pages/HubSpotActivityLog.jsx
+++ src/pages/HubSpotActivityLog.jsx
@@ -552,6 +552,8 @@ export default function HubSpotActivityLog() {
                           <SelectItem value="call">Call</SelectItem>
                           <SelectItem value="email">Email</SelectItem>
                           <SelectItem value="meeting">Meeting</SelectItem>
+                          <SelectItem value="task">Task</SelectItem>
+                          <SelectItem value="note">Note</SelectItem>
                         </SelectContent>
                       </Select>
                     </div>
