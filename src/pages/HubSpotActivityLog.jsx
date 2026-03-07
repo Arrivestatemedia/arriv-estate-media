14e88ddb6e8659e6140e6648bb8c76150c3931af
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-07 07:13:04 +0000

    File changes

--- src/pages/HubSpotActivityLog.jsx
+++ src/pages/HubSpotActivityLog.jsx
@@ -466,6 +466,7 @@ export default function HubSpotActivityLog() {
       picture_urls: formPictureUrls,
       contact_name: selectedContactObj?.name || "",
       contact_email: selectedContactObj?.email || "",
+      contact_phone: selectedContactObj?.phone || "",
       company_name: selectedContactObj?.company || "",
       sales_member_email: user?.email,
       sales_member_id: user?.id
