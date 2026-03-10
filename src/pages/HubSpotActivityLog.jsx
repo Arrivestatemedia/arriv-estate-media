3a596c5d7e1d45cc7c14c38eb639cf052aebb649
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-10 03:51:57 +0000

    File changes

--- src/pages/HubSpotActivityLog.jsx
+++ src/pages/HubSpotActivityLog.jsx
@@ -294,9 +294,10 @@ export default function HubSpotActivityLog() {
       .then(logs => {
         const uniqueContacts = {};
         logs?.forEach(log => {
-          if (log.contact_email && !uniqueContacts[log.contact_email]) {
-            uniqueContacts[log.contact_email] = {
-              email: log.contact_email,
+          const key = log.contact_email || log.contact_name;
+          if (key && !uniqueContacts[key]) {
+            uniqueContacts[key] = {
+              email: log.contact_email || "",
               name: log.contact_name,
               company: log.company_name
             };
