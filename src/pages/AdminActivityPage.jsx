8f46f2ab27bbd0fd7a4efee800624849583dd852
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-10 03:53:45 +0000

    File changes

--- src/pages/AdminActivityPage.jsx
+++ src/pages/AdminActivityPage.jsx
@@ -73,9 +73,10 @@ export default function AdminActivityPage({ user: propsUser, initialSubTab, onVi
       .then(logs => {
         const uniqueContacts = {};
         logs?.forEach(log => {
-          if (log.contact_email && !uniqueContacts[log.contact_email]) {
-            uniqueContacts[log.contact_email] = {
-              email: log.contact_email,
+          const key = log.contact_email || log.contact_name;
+          if (key && !uniqueContacts[key]) {
+            uniqueContacts[key] = {
+              email: log.contact_email || "",
               name: log.contact_name,
               company: log.company_name
             };
