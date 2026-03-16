da7c8b0541c4c74cde5ccc6608f42153412f4650
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-16 11:59:46 +0000

    File changes

--- src/pages/HubSpotActivityLog.jsx
+++ src/pages/HubSpotActivityLog.jsx
@@ -833,6 +833,7 @@ export default function HubSpotActivityLog() {
             <CardContent className="pt-6">
               <ContactSearch
                 salesMemberId={user?.id}
+                salesMemberEmail={user?.email}
                 openNewContactForm={openNewContactForm}
                 setOpenNewContactForm={setOpenNewContactForm}
                 prefilledData={prefilledContactData}
