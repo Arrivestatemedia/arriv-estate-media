3a127e313b4ba528d4cefa12e533df2e45e7913a
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-16 11:59:13 +0000

    File changes

--- src/pages/AdminActivityPage.jsx
+++ src/pages/AdminActivityPage.jsx
@@ -838,7 +838,8 @@ export default function AdminActivityPage({ user: propsUser, initialSubTab, onVi
           <Card style={{ backgroundColor: '#FFFFFF' }}>
             <CardContent className="pt-6">
               <ContactSearch 
-                salesMemberId={user?.id} 
+                salesMemberId={user?.id}
+                salesMemberEmail={user?.email}
                 openNewContactForm={openNewContactForm}
                 setOpenNewContactForm={setOpenNewContactForm}
                 prefilledData={prefilledContactData}
