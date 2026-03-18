8a388e116bb817efce6762bd85ce4e104513b12c
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-03-18 14:11:57 +0000

    File changes

--- functions/regenerateCallMap.ts
+++ functions/regenerateCallMap.ts
@@ -131,7 +131,7 @@ Page Views: ${contact.hs_analytics_num_page_views || 0}`;
 
     // Detect box/intro package sent and first contact
     const allActivityNotes = pastActivities.map(a => a.notes || "").join(" ");
-    const boxSent = /box sent/i.test(allActivityNotes);
+    const boxSent = /box sent|sent.*box|intro package|introduction package|mailed.*package|package.*sent|sent.*package/i.test(allActivityNotes);
     const isFirstContact = pastActivities.length === 0 || pastActivities.every(a => /^(FIRST CONTACT:|Contact created:)/i.test((a.notes || "").trim()));
     const firstName = (contactName || "").split(" ")[0] || "there";
 
