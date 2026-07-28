fd1b8919b2c5b9fd3e63405cf17e543da5206b33
Bradley Burke <bradcburke@arrivestatemedia.com>
2026-07-28 06:18:22 +0000

    File changes

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -170,17 +170,7 @@ export default function SignIn() {
             </Button>
 
           </form>
-          <div className="mt-4">
-            <Button
-              type="button"
-              variant="outline"
-              className="w-full border-[#B8956A]/40 text-[#B8956A] hover:bg-[#B8956A]/10"
-              onClick={() => navigate(createPageUrl("ApplicationPortal"))}
-            >
-              Check Application Status
-            </Button>
-          </div>
-          </CardContent>
+        </CardContent>
       </Card>
     </div>
   );
